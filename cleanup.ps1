fa8168a51caa56242de5540efa16d22c8cac293b
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-08-29 14:00:33 +1200

    Rename some of the README files

--- cleanup.ps1
+++ cleanup.ps1
@@ -4,7 +4,7 @@ if ((Test-Path 'Modulefile') -and (Test-Path 'manifests/init.pp') -and (Test-Pat
   Remove-Item 'Modulefile.orig'
   Remove-Item 'manifests/init.pp.orig'
   Remove-Item 'tests/init.pp.orig'
-  Remove-Item 'README1st.markdown'
+  Remove-Item 'puppet-blank.README.1st.markdown'
   Remove-Item '.fixtures.yml.orig'
   Remove-Item 'unblank.ps1'
   Remove-Item 'cleanup.ps1'
