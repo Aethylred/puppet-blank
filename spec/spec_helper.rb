f96ac66348e4c3edacd067dce500f95d924124b2
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-05-05 14:36:28 +1200

    Tests, but does not fail, for strict variables.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-augeas'
 RSpec.configure do |c|
   c.augeas_fixtures = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'augeas')
+  c.before :each do
+    if ENV['STRICT_VARIABLES'] == 'yes'
+        Puppet.settings[:strict_variables]=true
+    end
+  end
 end
